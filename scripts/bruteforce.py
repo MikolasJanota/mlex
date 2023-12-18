fadbc014f962ce2c6a68b6a891741abcb2efef13
mikolas <mikolas.janota@gmail.com>
2023-12-18 10:44:10 +0100

    small improvements to the bruteforce script

--- scripts/bruteforce.py
+++ scripts/bruteforce.py
@@ -4,18 +4,20 @@
 # Created on:  Sat Dec 16 19:25:47 CET 2023
 # Copyright (C) 2023, Mikolas Janota
 from itertools import permutations
+from statistics import mean
 import ast
 import sys
 import copy
-update_list = []
 
 def inv(pi):
+    """ Calculate the inverse of a given permutation. """
     rv = [-1] * len(pi)
     for i, v in enumerate(pi):
         rv[v] = i
     return rv
 
 def prn(g):
+    """ Pretty-print a binary table. """
     print('[', end='')
     ord=len(g)
     for rx,r in enumerate(g):
@@ -28,6 +30,7 @@ def prn(g):
     print(']', end='')
 
 def perm(g, pi, pi1):
+    """ Calculate the isomorphic copy of g given a permutation and its inverse. """
     ord = len(g)
     rng = range(ord)
     rv = [[-1] * ord for _ in rng]
@@ -37,6 +40,7 @@ def perm(g, pi, pi1):
     return rv
 
 def cmp(orig, ming, pi, pi1):
+    """ Compare the isomorphic copy of orig to ming. """
     rng = range(len(orig))
     for r in rng:
         for c in rng:
@@ -49,6 +53,7 @@ def cmp(orig, ming, pi, pi1):
     return 0
 
 def prn_perm(pi):
+    """ Pretty-print a permutation. """
     ord = len(pi)
     seen = set()
     rv = ''
@@ -65,36 +70,42 @@ def prn_perm(pi):
     return rv
 
 def solve(g):
+    """ Find the smallest isomorphic copy of g. """
     rng = range(len(g))
     m = copy.deepcopy(g)
     best_pi = [i for i in rng]
-    updates = 0
-    for pi in permutations(rng):
+    updates, last = 0,0
+    for i, pi in enumerate(permutations(rng)):
         pi1 = inv(pi)
         if cmp(g, m, pi, pi1) < 0:
+            last = i
             m = perm(g, pi, pi1)
             best_pi = copy.deepcopy(pi)
             updates += 1
     prn(m)
-    update_list.append(updates)
-    return best_pi
+    return updates, last, best_pi
 
 def run_main():
+    updates_list = []
+    last_list = []
     gs = ast.literal_eval(sys.stdin.read())
     print('[')
     for i,g in enumerate(gs):
         for rx,r in enumerate(g):
             for cx,c in enumerate(r):
                r[cx]-=1
-        pi = solve(g)
+        updates, last, pi = solve(g)
         if i+1 < len(gs):
            print(',', flush=True)
         else:
            print(flush=True)
-        print(f"# updates: {update_list[-1]}", file=sys.stderr, flush=True)
-        print(f"# pi: {prn_perm(pi)}")
+        print(f"# updates: {updates}", flush=True)
+        print(f"# pi {last}: {prn_perm(pi)}")
+        updates_list.append(updates)
+        last_list.append(last)
     print(']')
-    print(f"avg updates: {sum(update_list)/len(update_list)}", file=sys.stderr)
+    print(f"# avg updates: {mean(updates_list)}")
+    print(f"# avg permutation: {mean(last_list)}")
 
 if __name__ == "__main__":
     sys.exit(run_main())
